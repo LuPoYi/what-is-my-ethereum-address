949566675721965015a9f75d0f66ea2127616ebb
Bob <maydaybob2000@gmail.com>
2022-09-19 21:33:36 +0800

    Add @web3-react/core

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
-import React from "react"
-
 import "../styles/index.css"
 
 import { AppProps } from "next/app"
+import { Web3ReactProvider } from "@web3-react/core"
+import { ethers } from "ethers"
+
+const getLibrary = (provider) => {
+  return new ethers.providers.Web3Provider(provider)
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,49 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { InjectedConnector } from "@web3-react/injected-connector"
+import Layout from "../components/Layout"
+import Link from "next/link"
+import { ethers } from "ethers"
+import { useWeb3React } from "@web3-react/core"
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const injectedConnector = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42],
+})
+
+const IndexPage = () => {
+  const { active, account, library, chainId, activate, deactivate } =
+    useWeb3React()
+
+  const connectOnClick = () => activate(injectedConnector)
+  const disconnectOnClick = () => deactivate()
+
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Next.js 👋</h1>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+      <div>
+        {!active ? (
+          <button type="button" onClick={connectOnClick}>
+            connect
+          </button>
+        ) : (
+          <button type="button" onClick={disconnectOnClick}>
+            disconnect
+          </button>
+        )}
+      </div>
+      account: {account}
+      {/* 
+      <button
+        color="primary"
+        onClick={() => handleConnectWalletOnClick(injected)}
+      >
+        Connect
+      </button> */}
+    </Layout>
+  )
+}
 
 export default IndexPage
